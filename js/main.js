8b06a27d42e71f128df6651b45828b22f707cdaa
ira <vikhrova.irina@gmail.com>
2022-12-09 13:10:08 +0700

    js create 25 objects ref

--- js/main.js
+++ js/main.js
@@ -50,18 +50,17 @@ console.log(CheckStringLength('Привет!', 7));
 // 4. Создать массив из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
 
 
-// Вспомогательна функция, которая генерирует массив чисел от 1 до 25
-let GetRange = () => {
-  return  Array.from({ length: 25}, (_, i) => 1 + i);
-  };
 
-// Функция с массивом из 25-ти ID
-let IdList = GetRange().slice();
-console.log('Массив айди ', IdList);
+// Получаем  ID
+let GetID = (number) => {
+  return number;
+}
 
-// Функция, которая генерирует массив из 25-ти урл картинки
-let UrlList = GetRange().map(x => 'photos/' + x + '.jpg')
-console.log('массив урлов: ', UrlList);
+// Получаем урл картинки
+let GetUrl = (number) => {
+return ('photos/' + number + '.jpg');
+}
+
 
 // Массив с описаниями фотографий
 let descriptionList = ['Лето', 'Море', 'Коты', 'Дети', 'Дома','Реки', 'Ветки', 'Зима', 'Осень', 'Листья','Книги', 'Перья', 'Ноги', 'Руки', 'Окна','Звери', 'Люди', 'Кепки', 'Леди', 'Кони','Пони', 'Воры', 'Лень', 'Тень', 'День' ];
@@ -76,39 +75,39 @@ GetRandomNumber(0,200);
 let autorNames = [ 'Kolya', 'Petya' , 'Olya' , 'Vera'];
 let messageList = ['Всё отлично!',' В целом всё неплохо. Но не всё.', ' Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.' , 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
 
-let CreateComment = () => {
-
-  let commentsList = [];
-  for (let i = 1; i < 25; i++) {
-
-    commentsList.push(
-      {
-      id: i,
+let CreateComment = (id) => {
+      return {
+      id: id,
       avatar: 'img/avatar-'+ GetRandomNumber(1,6) +'.svg',
       message: messageList[GetRandomNumber(0, messageList.length - 1)],
       name: autorNames[GetRandomNumber(0, autorNames.length - 1)],
-      }
-    );
-  };
-  return commentsList;
+      };
 };
 
-console.log('Список комментариев' , CreateComment());
+
+let CreateCommentList = () => {
+let CommentsList = [];
+  for (let i = 1; i < GetRandomNumber(2,5) ; i++) {
+    CommentsList.push(CreateComment(i));
+  };
+return CommentsList;
+}
+
 
 
 // Функция, которая создаст конечный объект
 
 let CreatePost = () => {
   let objectsList = [];
-  for (let i = 0; i < 25 ; i++) {
+  for (let i = 1; i <= 15 ; i++) {
 
     objectsList.push(
       {
-      id: IdList[i],
-      url: UrlList[i],
-      description: descriptionList[i],
+      id: GetID(i),
+      url: GetUrl(i),
+      description: descriptionList[GetRandomNumber(0,24)],
       likes: GetRandomNumber(0,200),
-      comments: CreateComment()[i],
+      comments: CreateCommentList(),
       }
     );
   };
