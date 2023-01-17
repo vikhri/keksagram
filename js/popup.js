3afc9c750c7403dca74c71b305f9da92f5dd26c9
ira <vikhrova.irina@gmail.com>
2023-01-17 13:54:21 +0700

    wip pop up

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,36 @@
 import './pictures.js';
+import { getNewPost } from './pictures.js';
 
 let previews = document.querySelectorAll('.picture');
+let likes = document.querySelectorAll('.picture__likes');
+let comments = document.querySelectorAll('.picture__comments');
 let modalWindow = document.querySelector('.overlay');
-let bigPictureSrc = document.querySelector('.big-picture__img').children[0].src;
-console.log(bigPictureSrc)
-console.log(previews[1].children[0].src);
+let bigPicture = document.querySelector('.big-picture__img').children[0];
+let likesNumber = modalWindow.querySelector('.likes-count');
+let commentsNumber = modalWindow.querySelector('.comments-count');
+let commentsCount = modalWindow.querySelector('.social__comment-count');
+let commentsLoader = modalWindow.querySelector('.comments-loader');
 
 
-let showPopup = function (preview) {
+
+let showPopup = function (preview, likes, comments) {
       preview.addEventListener('click', function() {
-      console.log('тыкнули на картинку №' + preview.id)
       modalWindow.classList.remove('hidden');
+      bigPicture.src = preview.children[0].src;
+      likesNumber.textContent = likes.textContent;
+      commentsNumber.textContent = comments.textContent;
+  });
 
-    });
   };
 
   for (let i = 0; i < previews.length; i++) {
-    showPopup(previews[i]);
+    showPopup(previews[i], likes[i], comments[i]);
+
   }
 
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
 
+  document.querySelector('body').classList.add('modal-open');
 
-
-// не забыть принять новую разметку.
 
